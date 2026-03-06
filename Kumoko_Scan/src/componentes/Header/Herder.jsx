21cf175e6002ba6b9ffb5f9e6ff9fcac7508324a
PFLYIN <pedrodiegomeira@gmail.com>
2026-03-05 22:35:17 -0300

    começando o header

---
+++ Kumoko_Scan/src/componentes/Header/Herder.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+    return (
+        <header className="header">
+            <div className="header-logo">
+                <span className="kumoko-logo">AAAA</span>
+                <h1>Kumoko<span>Scan</span></h1>
+            </div>
+        </header>
+    )
+}
