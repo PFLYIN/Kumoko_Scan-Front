887f4e331c953d3c90420c187b9ce4f803a314c5
PFLYIN <pedrodiegomeira@gmail.com>
2026-03-11 17:48:57 -0300

    FFFF

--- Kumoko_Scan/src/componentes/Header/Header.jsx
+++ Kumoko_Scan/src/componentes/Header/Header.jsx
@@ -18,21 +18,25 @@ function Header({ onSearch }) {
                 <div className="logo">
                     <img src={logo} alt="kumoko_logo.png" />
                 </div>
-                <nav className="main-nav">
-                    <ul>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/">Mangás</Link></li>
-                        <li><Link to="/">Livros</Link></li>
-                        <li><Link to="/">Projetos</Link></li>
-                        <li><Link to="/">Sobre</Link></li>
-                        <li><Link to="/">Contato</Link></li>
-                    </ul>
-                    <div className="search-box">
-                        <span>Search</span>
-                        <i className="fas fa-search"></i>
-                    </div>
-                    <button className="donate-btn">Donate</button>
-                </nav>
+  <nav className="main-nav">
+
+<ul>
+<li><Link to="/">Home</Link></li>
+<li><Link to="/">Mangás</Link></li>
+<li><Link to="/">Livros</Link></li>
+<li><Link to="/">Projetos</Link></li>
+<li><Link to="/">Sobre</Link></li>
+<li><Link to="/">Contato</Link></li>
+</ul>
+
+<div className="search-box">
+<span>Search</span>
+<i className="fas fa-search"></i>
+</div>
+
+<button className="donate-btn">Donate</button>
+
+</nav>
             </div>
         </header>
     );
--- Kumoko_Scan/src/pages/Home/Home.jsx
+++ Kumoko_Scan/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import './Home.css';
 import { Header } from "../../componentes";
 import { Footer } from "../../componentes";
 import destaque from "../../assets/imagentest2.jpg";
+import card1 from "../../assets/imagentest1.jpg";
 
 function Home() {
     return (
@@ -14,15 +15,47 @@ function Home() {
                 <a href="#"><i className="fab fa-discord"></i></a>
             </aside>
             <section className='hero-section'>
+                <img src={destaque} alt="imagem destaque" />
                 <div className='hero-overlay'></div>
-                <img src={destaque} alt="imagentest2.jpg"/>
                 <div className='hero-content'>
                      <h1>Trazendo o mundo para você, um cápitulo por vez</h1>
-                     
+                      <a href="#" className="about-btn">SOBRE NÓS <i className="fas fa-arrow-right"></i></a>
                 </div>
             </section>
-         </main>
+
+            <section className='card-section'>
+                <div className='card card-1'>
+                    <div className='card-overlay'></div>
+                       <img src={card1} alt="imagentest1.jpg"/>
+                    <div className='card-content'>
+                        <h3>ABOUT US</h3>
+                    </div>
+                </div>
+                <div className='card card-2'>
+                    <div className='card-overlay'></div>
+                        <img src={card1} alt="imagentest1.jpg"/>
+                    <div className='card-content'>
+                        <h3>OUR TEAM</h3>
+                    </div> 
+                </div>
+                <div className='card card-3'>
+                    <div className='card-overlay'></div>
+                        <img src={card1} alt="imagentest1.jpg"/>
+                    <div className='card-content'>
+                        <h3>PROJECTS</h3>
+                    </div> 
+                </div>
+                <div className='card card-4'>
+                    <div className='card-overlay'></div>
+                        <img src={card1} alt="imagentest1.jpg"/>
+                    <div className='card-content'>
+                        <h3>GET INVOLVED</h3>
+                    </div> 
+                </div>
+            </section>
+
+        </main>
     );
-}
+};
 
 export default Home;
