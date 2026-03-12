85be15f33ca8809895b96e81d3763719600715fa
PFLYIN <pedrodiegomeira@gmail.com>
2026-03-11 22:47:16 -0300

    isso!

--- Kumoko_Scan/src/componentes/Footer/Footer.jsx
+++ Kumoko_Scan/src/componentes/Footer/Footer.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import "./Footer.css";
 
 function Footer() {
-  return (
-    <footer></footer>
-  );
+    return (
+        <aside className="social-sidebar">
+
+            <a href="#" className="social-link">
+                <i className="fab fa-facebook-f"></i>
+            </a>
+
+            <a href="#" className="social-link">
+                <i className="fab fa-twitter"></i>
+            </a>
+
+            <a href="#" className="social-link">
+                <i className="fab fa-instagram"></i>
+            </a>
+
+            <a href="#" className="social-link">
+                <i className="fab fa-discord"></i>
+            </a>
+
+        </aside>
+    );
 }
 
 export default Footer;
--- Kumoko_Scan/src/pages/Home/Home.jsx
+++ Kumoko_Scan/src/pages/Home/Home.jsx
@@ -7,15 +7,10 @@ import card1 from "../../assets/imagentest1.jpg";
 
 function Home() {
     return (
+        
          <main className='main-content'>
-            <aside className='social-sidebar'>
-                <a href="#"><i className="fab fa-facebook-f"></i></a>
-                <a href="#"><i className="fab fa-twitter"></i></a>
-                <a href="#"><i className="fab fa-instagram"></i></a>
-                <a href="#"><i className="fab fa-discord"></i></a>
-            </aside>
-            <section className='hero-section'>
-                <img src={destaque} alt="imagem destaque" />
+            
+<section className='hero-section' style={{ backgroundImage: `url(${destaque})` }}>
                 <div className='hero-overlay'></div>
                 <div className='hero-content'>
                      <h1>Trazendo o mundo para você, um cápitulo por vez</h1>
@@ -55,6 +50,7 @@ function Home() {
             </section>
 
         </main>
+
     );
 };
 
